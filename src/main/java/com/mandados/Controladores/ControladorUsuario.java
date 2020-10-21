d1f445ab5254c4a8d33148810d8b726e0e6e7ed0
cirilo mendoza <cirilomendozaescamilla@gmail.com>
2020-10-21 15:43:40 -0500

    Prueba de error del registro de restaurante

--- src/main/java/com/mandados/Controladores/ControladorUsuario.java
+++ src/main/java/com/mandados/Controladores/ControladorUsuario.java
@@ -1,10 +1,52 @@
 package com.mandados.Controladores;
 
+import com.mandados.Entidades.Repartidor;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class ControladorUsuario {
-    
+    // @Autowired
+    // private IRestauranteService servicerestaurante;
+
+    @GetMapping("/resrep")
+    public String registroRepartidor(Model model){
+    	// model.addAttribute("userrepartidor", new Repartidor());
+        return "/registro/repartidor";
+    }
+
+    @PostMapping("/register_repartidor")
+	public String save(@Validated Repartidor r, Model model){
+        // servicerestaurante.save(r);
+        // User newuserrestaurante = new User();
+        // newuserrestaurante.setUsername(r.getCorreo());
+        // newuserrestaurante.setEnabled(true);
+        // Passgenerator ps = new Passgenerator();
+        // String generada = aleatorio();
+        // String password = ps.getPassword(generada);
+        // newuserrestaurante.setPassword(password);
+
+        // List<Authority> lista = authorityRepository.findAll();
+        // ArrayList<Authority> list = new ArrayList<Authority>();
+        // for(int i=0;i<lista.size();i++){
+        //     if(lista.get(i).getAuthority().equals("ROL_RESTAURANTE")){
+        //         list.add(lista.get(i));
+        //         // System.out.println("Respuesta => " + lista.get(i).getAuthority());
+        //     }
+        // }
+        // newuserrestaurante.setAuthority(list);
+        // // System.out.println("List => " + list);
+        // String body = "Estimado usuario, \n  Gracias por contactarnos al correo \n " +
+        // "Los datos de acceso son: \n user: El correo que usó en el registro.\n " + 
+        // "contraseña: " + generada;
+        // emailService.sendEmail(r.getCorreo(), body, "¡¡¡¡GRACIAS!!!!");
+        // serviceuser.save(newuserrestaurante);
+		return "redirect:/";
+    }
 }
---
+++ src/main/java/com/mandados/Entidades/Repartidor.java
@@ -0,0 +1,54 @@
+package com.mandados.Entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Repartidor {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String nombre;
+    
+    @Column
+    private String correo;
+    
+    @Column
+    private String telefono;
+
+	@Override
+	public String toString() {
+		return "Restaurante [id=" + id + ", nombre=" + nombre + ", correo=" + correo + ", colonia=" + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String telefono) {
+		this.telefono = telefono;
+	}
+
+}
