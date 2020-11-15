eb6bfd08673fb6cc8911086f8168f670c2b123bd
cirilo mendoza <cirilomendozaescamilla@gmail.com>
2020-11-15 16:59:11 -0600

    modificando la lista de los productos

--- src/main/java/com/mandados/Repository/ProductoRepository.java
+++ src/main/java/com/mandados/Repository/ProductoRepository.java
@@ -1,6 +1,4 @@
 package com.mandados.Repository;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
