11767dd8dab4b004fad6f7c0ea6eef914073234a
kevin <velasquezruizkevin@gmail.com>
2021-02-05 20:33:39 -0600

    " "

--- src/main/resources/static/dist/js/funcionesextra.js
+++ src/main/resources/static/dist/js/funcionesextra.js
@@ -281,7 +281,7 @@ async function confirmarPedidoSincomercio(){
 									,
 				success: function(res){
 					window.location.href = "/listapedido";
-					Swal.fire('insertado correctamente!', '', 'success');
+					Swal.fire('insertado correctamente! seras redirigido a los PEDIDOS para que asignes repartidor', '', 'success');
 						$("#nuevo").modal('hide');//ocultamos el modal
 						$('body').removeClass('modal-open');//eliminamos la clase del body para poder hacer scroll
 						$('.modal-backdrop').remove();//eliminamos el backdrop del modal
